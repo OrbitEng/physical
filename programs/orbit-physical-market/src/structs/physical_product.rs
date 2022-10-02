6a3288ae32cd07ba5c023f3b65e17cfefd40f26e
indianeros58 <0xDEADBEAT@gmail.com>
2022-10-02 04:49:03 -0400

    fuck

--- programs/orbit-physical-market/src/structs/physical_product.rs
+++ programs/orbit-physical-market/src/structs/physical_product.rs
@@ -1,6 +1,7 @@
 use product::product_struct::OrbitProduct;
 use anchor_lang::prelude::*;
 
+// disc a81c2d2c [168, 28, 45, 44, 211, 241, 238, 140]
 #[account]
 pub struct PhysicalProduct{
     pub metadata: OrbitProduct, // 38 as of beta
